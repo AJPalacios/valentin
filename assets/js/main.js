f5b3c42a9182be773c49a19f94931fc0a2517d00
DrLfnt <dr.lfnt@gmail.com>
2018-01-30 00:17:30 -0600

    .min
    se comprimieron los codigos de css y de js

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,11 +3,7 @@ alert("conectado")
 function fondo(){
   var el = document.getElementById('#ecommerce');
   var arrowNext = document.getElementByClassName("controlArrow next")
-   
   arrowNext.addEventListener("click",()=>{
     el.classList.add('background-perfume');
   });
-
-    
-
 }
